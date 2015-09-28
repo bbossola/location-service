64f618e27261fd25946a00df7cc0cb1da2f714c7
Berta-G <berta.gutierrez@gmail.com>
2015-09-28 15:08:06 +0100

    location finder added

--- app/LocationFinder.js
+++ app/LocationFinder.js
@@ -1,12 +1,39 @@
 'use strict';
+var geolite = require('node-geolite2');
+geolite.init();
 
 function LocationFinder() {
 
 }
 
 LocationFinder.prototype = {
-    lookup: function() {
+    lookup: function(ip) {
 
+      return ip.map( function(ip){
+        try {
+          var result = geolite.getGeoDataSync(ip);
+        }
+        catch(e) {
+          return {host: ip, error: 'Unknown host: ' + ip};
+        }
+        return this.serialize(result, ip);
+      }, this);
+
+    },
+    serialize: function(geolite_response, ip) {
+      if(geolite_response) {
+        var result = {country:{}};
+        result.country.language = 'en';
+        result.country.iso_code = geolite_response.country.iso_code;
+        result.country.name = geolite_response.country.names.en;
+        result.host = ip;
+      } else {
+        var result = {};
+        result.host = ip;
+        result.error = "The address " + ip + " is not in the database."
+      };
+      
+      return result;
     }
 };
 
